2b6111ebcb83c2ac56b1e92ed034f0963a89bda1
Pascal Sturmfels <psturm@umich.edu>
2016-10-30 19:33:33 -0400

    Finished the setting up phase

---
+++ CrashyPlane-settingUp/CrashyPlane/GameScene.swift
@@ -0,0 +1,19 @@
+//
+//  GameScene.swift
+//  CrashyPlane
+//
+//  Created by Pascal Sturmfels on 10/30/16.
+//  Copyright © 2016 KTP. All rights reserved.
+//
+
+import SpriteKit
+import GameplayKit
+
+class GameScene: SKScene {
+    
+    override func didMove(to view: SKView) {
+    }
+
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+    }
+}
